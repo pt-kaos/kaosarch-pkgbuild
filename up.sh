99fe4857dc92b41b6dd374cc9532f41fb2f32c18
Pedro Teodoro <pedro_teodoro@hotmail.com>
2025-07-14 23:50:38 +0100

    update

--- up.sh
+++ up.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 set -eo pipefail
 ##################################################################################################################
-# Author    : Erik Dubois
-# Youtube   : https://youtube.com/erikdubois
-##################################################################################################################
-# Changed by: Pedro Teodoro
+# Author    : Pedro Teodoro
 # Github    : https://github.com/pt-kaos
-# Website   : 
-#
-##################################################################################################################
-#
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
-#
 ##################################################################################################################
 #tput setaf 0 = black
 #tput setaf 1 = red
@@ -24,26 +15,15 @@ set -eo pipefail
 #tput setaf 8 = light blue
 ##################################################################################################################
 
-# variables and functions
 workdir=$(pwd)
-#dir="calamares-3.3.14.r39.g274996f"
-#source=" /home/pedro/Programing/ArchISOs/KaosArch/kaosarch-pkgbuild/"
-#destiny="/home/pedro/Programing/ArchISOs/KaosArch/kaosarch-calamares-config/etc/calamares/pkgbuild/"
-#
-###################################################################################################################
-#
-#rm -r $destiny*
-#cp -r $source$dir/* $destiny
 
-# Below command will backup everything inside the project folder
+# Send Everything to Github
 git add --all .
 
 # Committing to the local repository with a message containing the time details and commit text
-
 git commit -m "update"
 
 # Push the local files to github
-
 branch=$(git rev-parse --abbrev-ref HEAD)
 git push -u origin "$branch"
 
